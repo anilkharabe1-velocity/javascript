11a72fb659fde2fd5996ede7d19c8bf6e0ab7e72
anilkharabe1-velocity <anilkharabe1@gmail.com>
2026-07-09 09:04:09 +0530

    controlled statement

---
+++ ifElseStatement.js
@@ -0,0 +1,11 @@
+let age = prompt("Enter your age");
+age = Number(age);
+
+// controlled statement
+if (age >= 18) {
+  console.log("you are eligible for voting");
+} else {
+  console.log("you are not eligible for voting");
+}
+
+console.log("your age is ", age);
